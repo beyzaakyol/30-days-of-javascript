646e61a97775c363813719f7746b37a4da1342f1
beyzaakyol <akyolbeyza99@gmail.com>
2022-02-01 22:41:46 +0300

    Add day 1

---
+++ Day1/introduction.js
@@ -0,0 +1,20 @@
+//console.log('bu yazı introduction.js den geldi!')
+
+console.log( typeof 'prototurk' )
+console.log( typeof 5 )
+console.log( typeof true )
+console.log( typeof null )
+console.log( typeof undefined )
+
+let youtubeChannel = 'prototurk',
+   channelAge = 11,
+   isChannelStillActive = true,
+   secondChannel = null,
+   channelMoney = undefined
+
+const GRAVITY = 9.81
+const PI =3.14
+
+console.log(youtubeChannel, channelAge, isChannelStillActive, secondChannel, channelMoney)
+
+console.log(PI, GRAVITY)
